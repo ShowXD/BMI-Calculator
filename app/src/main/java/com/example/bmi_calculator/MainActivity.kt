0305eaac080ed222a4362d63e213061ca93584ff
ShowXD <n16418113@gmail.com>
2019-10-31 13:33:20 +0800

    Set transition animation

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.bmi_calculator
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("123")
+
+        val intent = Intent(this, ResultActivity::class.java)
+
+        btnSet(intent)
+    }
+
+    private fun btnSet(intent: Intent) {
+
+        buttonStart.setOnClickListener {
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+        }
     }
 }
